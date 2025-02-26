2c1a3dc558311c08f1a519f2e83ecb6c4706ddfd
amit <amit@4m17.io>
2025-02-26 00:41:18 -0500

    Secrets access

--- run_alpaca_recorder.py
+++ run_alpaca_recorder.py
@@ -1,13 +1,22 @@
-import os
+from google.cloud import secretmanager
 
 from recorders.alpaca_recorder import AlpacaSnapshotRecorder
 from persistence.gcp_cloud_storage import GCSPersistence
 
+PROJECT_ID = "market-data-model-20"
+
+
+def access_secret(project_id, secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
 
 def main():
     config = {
-        "key": os.environ.get("ALPACA_KEY"),
-        "secret": os.environ.get("ALPACA_SECRET"),
+        "key": access_secret(PROJECT_ID, "ALPACA_KEY"),
+        "secret": access_secret(PROJECT_ID, "ALPACA_SECRET"),
     }
 
     pl = GCSPersistence(
