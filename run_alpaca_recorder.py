9398884f15e19a3a05b17a384c2c6107f4412921
amit <amit@4m17.io>
2025-02-26 00:56:02 -0500

    Same

--- run_alpaca_recorder.py
+++ run_alpaca_recorder.py
@@ -6,9 +6,9 @@ from persistence.gcp_cloud_storage import GCSPersistence
 PROJECT_ID = 797853389585
 
 
-def access_secret(project_id, secret_id, version_id="latest"):
+def access_secret(project_id, secret_id, version_id="1"):
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}"
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(request={"name": name})
     return response.payload.data.decode("UTF-8")
 
