b492209890309b4e233d7d9159c7545774ac0145
amit <amit@4m17.io>
2025-02-26 00:55:25 -0500

    Trying fixing the secret loader

--- run_alpaca_recorder.py
+++ run_alpaca_recorder.py
@@ -8,7 +8,7 @@ PROJECT_ID = 797853389585
 
 def access_secret(project_id, secret_id, version_id="latest"):
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    name = f"projects/{project_id}/secrets/{secret_id}"
     response = client.access_secret_version(request={"name": name})
     return response.payload.data.decode("UTF-8")
 
